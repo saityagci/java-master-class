4256766d8cfebe673af3dd9e31c87196da7efaeb
sfaai <saityagci@gmail.com>
2026-08-31 02:59:29 -0400

    feat: add domain POJOs and enums (User, Car, CarBooking, Brand, BookingStatus)

---
+++ src/main/java/com/sait/Main.java
@@ -0,0 +1,8 @@
+package com.sait;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java Master Class");
+    }
+}
---
+++ src/main/java/com/sait/car/Brand.java
@@ -0,0 +1,5 @@
+package com.sait.car;
+
+public enum Brand {
+    TESLA, AUDI, MERCEDES, TOYOTA;
+}
